4b0935d993c556cb88381e4befe22f8363b7e9fe
Ido Shitrit <77319162+ido7746@users.noreply.github.com>
2021-06-12 21:34:58 +0300

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -1,6 +1,6 @@
 #include "Function.h"
 
-void OOP_Hw3::Function::MargeVatiable(std::list<std::string> listR, std::list<std::string> listL)//מחבר את רשימות הערכים עם הרשימות המתקבלות, ללא כפילויות
+void OOP_Hw3::Function::MargeVatiable(std::list<std::string> listR, std::list<std::string> listL)
 {
     this->variables.merge(listR);
     this->variables.merge(listL);
@@ -13,9 +13,9 @@ void OOP_Hw3::Function::MargeVatiable(std::list<std::string> listR, std::list<st
 std::map<std::string, std::map<std::string, SharedPtr<OOP_Hw3::Function> > > OOP_Hw3::Function::Hessian() const
 {
     std::map<std::string, std::map<std::string, SharedPtr<OOP_Hw3::Function> > > hessianMatrix;
-    for(std::_List_const_iterator<std::string > i = this->variables.begin(); i != this->variables.end(); ++i){//רץ על כל רשימת הערכים
+    for(std::_List_const_iterator<std::string > i = this->variables.begin(); i != this->variables.end(); ++i){
         SharedPtr<OOP_Hw3::Function> firsDeriveByi =  this->DeriveBy(*i);//נגזרת ראשונה לפי המשתנה הראשון
-        for(std::_List_const_iterator<std::string > j = this->variables.begin(); j != this->variables.end(); ++j){//רץ עוד פעם על רשימת הערכים
+        for(std::_List_const_iterator<std::string > j = this->variables.begin(); j != this->variables.end(); ++j){
             SharedPtr<OOP_Hw3::Function> secDeriveByj = firsDeriveByi->DeriveBy(*j);//גוזר לפי המשתנה השני
             hessianMatrix[*i][*j] = secDeriveByj;
         }
